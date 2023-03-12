b08c291c33689f92d822433d8bd017823501795c
beratozcan <118129636+beratozcan@users.noreply.github.com>
2023-03-12 09:32:22 +0300

    Add files via upload

---
+++ movieapp/movies/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from .models import Category, Movie
+
+# Create your views here.
+
+kategori_liste = ["macera","romantik","dram"]
+film_liste = [
+    {
+        "id":1,
+        "film_adi": "film 1",
+        "aciklama": "film 1 aciklama",
+        "resim": "1.jpeg",
+        "anasayfa":True
+    },
+    {
+        "id":2,
+        "film_adi": "film 2",
+        "aciklama": "film 2 aciklama",
+        "resim": "2.jpeg",
+        "anasayfa":True
+
+    },
+    {
+        "id":3,
+        "film_adi": "film 3",
+        "aciklama": "film 3 aciklama",
+        "resim": "3.jpeg",
+        "anasayfa":False
+    },
+    {
+        "id":4,
+        "film_adi": "film 4",
+        "aciklama": "film 4 aciklama",
+        "resim": "4.jpeg",
+        "anasayfa":False
+    }
+
+]
+
+def home(request):
+    data = {
+        "kategoriler":Category.objects.all(),
+        "filmler": Movie.objects.filter(anasayfa=True)
+    }
+    return render(request,"index.html",data)
+
+def movies(request):
+    data = {
+        "kategoriler":Category.objects.all(),
+        "filmler": Movie.objects.all()
+    }
+    return render(request,"movies.html",data)
+
+def moviedetails(request,id):
+
+    data = {
+        "movie" : Movie.objects.get(id=id)
+    }
+
+    return render(request,"details.html",data)
+
